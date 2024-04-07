cf7fc040d659578f82aee2e76288e54c1f82b3f9
pujamavadhiya <pujamavadhiya12@gmail.com>
2024-04-07 05:46:33 +0530

    added goals page

--- src/pages/Budget/Goals.jsx
+++ src/pages/Budget/Goals.jsx
@@ -1,37 +1,138 @@
-import React from "react";
-import { Fragment, useState } from "react";
-import { TransAPI } from "../../api";
-import moment from "moment";
+// import React from "react";
+// import { Fragment, useState } from "react";
+// import { TransAPI } from "../../api";
+// import moment from "moment";
+
+// export default function Example() {
+//   React.useEffect(() => {
+//     document.title = "Dashboard";
+//   });
+//   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+//   return (
+//     <>
+//       <div className="flex h-full flex-col">
+//         {/* Bottom section */}
+//         <div className="flex min-h-0 flex-1 overflow-hidden">
+//           <main className="min-w-0 flex-1 border-t border-gray-200 lg:flex">
+//             <section
+//               aria-labelledby="primary-heading"
+//               className="pl-10 pr-10 mt-10 flex bg-white h-full min-w-0 flex-1 flex-col overflow-y-auto lg:order-last"
+//             >
+//               <div className="border-b border-gray-200 pb-5">
+//                 <h1 className="text-4xl font-medium leading-6 text-gray-900">
+//                   Your Goals ✨
+//                 </h1>
+//                 <p className="mt-5 max-w-4xl text-sm text-gray-500">
+//                     A list of your financial goals,  Create actionable plans to achieve these goals, including budgeting strategies and investment recommendations
+//                     or specific goals like saving for a house or retirement, saving for a vacation, buying a home, or paying off debt.
+//                 </p>
+//               </div>
+//               <div>
+                
+//               </div>
+//             </section>
+//           </main>
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+
+
+
+import { CheckIcon } from '@heroicons/react/20/solid'
+
+const steps = [
+  { name: 'Create account', description: 'Vitae sed mi luctus laoreet.', href: '#', status: 'complete' },
+  {
+    name: 'Profile information',
+    description: 'Cursus semper viverra facilisis et et some more.',
+    href: '#',
+    status: 'current',
+  },
+  { name: 'Business information', description: 'Penatibus eu quis ante.', href: '#', status: 'upcoming' },
+  { name: 'Theme', description: 'Faucibus nec enim leo et.', href: '#', status: 'upcoming' },
+  { name: 'Preview', description: 'Iusto et officia maiores porro ad non quas.', href: '#', status: 'upcoming' },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function Example() {
-  React.useEffect(() => {
-    document.title = "Dashboard";
-  });
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
-
   return (
-    <>
-      <div className="flex h-full flex-col">
-        {/* Bottom section */}
-        <div className="flex min-h-0 flex-1 overflow-hidden">
-          <main className="min-w-0 flex-1 border-t border-gray-200 lg:flex">
-            <section
-              aria-labelledby="primary-heading"
-              className="pl-10 pr-10 mt-10 flex bg-white h-full min-w-0 flex-1 flex-col overflow-y-auto lg:order-last"
-            >
-              <div className="border-b border-gray-200 pb-5">
-                <h1 className="text-4xl font-medium leading-6 text-gray-900">
-                  Your Goals ✨
-                </h1>
-                <p className="mt-5 max-w-4xl text-sm text-gray-500">
-                    A list of your financial goals,  Create actionable plans to achieve these goals, including budgeting strategies and investment recommendations
-                    or specific goals like saving for a house or retirement, saving for a vacation, buying a home, or paying off debt.
-                </p>
-              </div>
-            </section>
-          </main>
-        </div>
-      </div>
-    </>
-  );
+<div>
+    <div className="border-b border-gray-200 pb-5 ml-10 mt-10">
+                 <h1 className="text-4xl font-medium leading-6 text-gray-900">
+                   Your Goals ✨
+                 </h1>
+                 <p className="mt-5 max-w-4xl text-sm text-gray-500">
+                     A list of your financial goals,  Create actionable plans to achieve these goals, including budgeting strategies and investment recommendations
+                     or specific goals like saving for a house or retirement, saving for a vacation, buying a home, or paying off debt.
+                 </p>
+               </div>
+    <nav aria-label="Progress" className="ml-12 mt-6">
+      <ol role="list" className="overflow-hidden">
+        {steps.map((step, stepIdx) => (
+          <li key={step.name} className={classNames(stepIdx !== steps.length - 1 ? 'pb-10' : '', 'relative')}>
+            {step.status === 'complete' ? (
+              <>
+                {stepIdx !== steps.length - 1 ? (
+                  <div className="absolute top-4 left-4 -ml-px mt-0.5 h-full w-0.5 bg-gray-900" aria-hidden="true" />
+                ) : null}
+                <a href={step.href} className="group relative flex items-start">
+                  <span className="flex h-9 items-center">
+                    <span className="relative z-10 flex h-8 w-8 items-center justify-center rounded-full bg-gray-900 group-hover:bg-gray-900">
+                      <CheckIcon className="h-5 w-5 text-white" aria-hidden="true" />
+                    </span>
+                  </span>
+                  <span className="ml-4 flex min-w-0 flex-col">
+                    <span className="text-sm font-medium">{step.name}</span>
+                    <span className="text-sm text-gray-500">{step.description}</span>
+                  </span>
+                </a>
+              </>
+            ) : step.status === 'current' ? (
+              <>
+                {stepIdx !== steps.length - 1 ? (
+                  <div className="absolute top-4 left-4 -ml-px mt-0.5 h-full w-0.5 bg-gray-300" aria-hidden="true" />
+                ) : null}
+                <a href={step.href} className="group relative flex items-start" aria-current="step">
+                  <span className="flex h-9 items-center" aria-hidden="true">
+                    <span className="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-900 bg-white">
+                      <span className="h-2.5 w-2.5 rounded-full bg-gray-900" />
+                    </span>
+                  </span>
+                  <span className="ml-4 flex min-w-0 flex-col">
+                    <span className="text-sm font-medium text-gray-900">{step.name}</span>
+                    <span className="text-sm text-gray-500">{step.description}</span>
+                  </span>
+                </a>
+              </>
+            ) : (
+              <>
+                {stepIdx !== steps.length - 1 ? (
+                  <div className="absolute top-4 left-4 -ml-px mt-0.5 h-full w-0.5 bg-gray-300" aria-hidden="true" />
+                ) : null}
+                <a href={step.href} className="group relative flex items-start">
+                  <span className="flex h-9 items-center" aria-hidden="true">
+                    <span className="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 border-gray-300 bg-white group-hover:border-gray-400">
+                      <span className="h-2.5 w-2.5 rounded-full bg-transparent group-hover:bg-gray-300" />
+                    </span>
+                  </span>
+                  <span className="ml-4 flex min-w-0 flex-col">
+                    <span className="text-sm font-medium text-gray-500">{step.name}</span>
+                    <span className="text-sm text-gray-500">{step.description}</span>
+                  </span>
+                </a>
+              </>
+            )}
+          </li>
+        ))}
+      </ol>
+    </nav>
+    </div>
+  )
 }
